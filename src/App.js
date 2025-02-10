17c8ae3bf6b5f082e3c472667bf3a8613f41b0bd
Logan Valeski <loganvaleski@gmail.com>
2025-02-10 17:59:21 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          We save photos, letters, and videos of our loved ones.<br />
+          Why not their voices?
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
